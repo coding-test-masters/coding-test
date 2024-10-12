3c1539577b1467183f3c37b2f1e6be182c814e82
IM-GYURI <kgulr0517@gmail.com>
2024-10-12 20:36:29 +0900

    LeetCode no.125 Valid Palindrome : 풀이 추가

--- src/Gyuri_125.java
+++ src/Gyuri_125.java
@@ -25,6 +25,30 @@ public class Gyuri_125 {
     }
   }
 
+  public static boolean isPalindrome2(String s) {
+    StringBuilder sb = new StringBuilder();
+
+    for (char c : s.toCharArray()) {
+      if (Character.isAlphabetic(c) || Character.isDigit(c)) {
+        sb.append(Character.toLowerCase(c));
+      }
+    }
+
+    int left = 0;
+    int right = sb.toString().length() - 1;
+
+    while (left < right) {
+      if (sb.charAt(left) != sb.charAt(right)) {
+        return false;
+      }
+
+      left++;
+      right--;
+    }
+
+    return true;
+  }
+
   public static void main(String[] args) {
     String s = "A man, a plan, a canal: Panama";
     System.out.println(isPalindrome(s));
