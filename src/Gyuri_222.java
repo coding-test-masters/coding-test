d9208f6bea6ee97c73b51e3d4d6478588316b45f
IM-GYURI <kgulr0517@gmail.com>
2024-10-24 10:32:08 +0900

    LeetCode no.222 Count Complete Tree Nodes

---
+++ src/Gyuri_222.java
@@ -0,0 +1,9 @@
+public class Gyuri_222 {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
+}
