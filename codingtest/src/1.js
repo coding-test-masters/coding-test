805539a1ebdecd186b1993f487e4cbff2ee5ca76
Marcel Lee <chungchung1315@gmail.com>
2025-02-18 00:03:32 +0900

    LeetCode no.1 Two Sum

---
+++ codingtest/src/1.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+      for (let j = i; j < nums.length; j++) {
+          if (i === j) {
+              continue;
+          }
+          if (nums[i] + nums[j] === target) {
+              return [i, j];
+          }
+      }
+  }
+  throw new Error();
+};
