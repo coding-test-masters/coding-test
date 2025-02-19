3afd230d4f7e76d820f57868cf81b186ca79bf30
Marcel Lee <chungchung1315@gmail.com>
2025-02-20 00:56:32 +0900

    LeetCode no.219 Contains Duplicate II

---
+++ codingtest/src/219.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+  for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+          if (j - i > k) {
+              break;
+          }
+          if (nums[i] === nums[j]) {
+              return true;
+          }
+      }
+  }
+  return false;
+};
